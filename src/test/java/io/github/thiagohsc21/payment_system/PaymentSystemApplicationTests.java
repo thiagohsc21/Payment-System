fcda65b08744896eafee29dca45c8eca11833a13
Thiago Henrique Cardoso <thiagohsc21@gmail.com>
2025-11-17 09:01:03 -0300

    Initial commit: Project skeleton

---
+++ src/test/java/io/github/thiagohsc21/payment_system/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.thiagohsc21.payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
